1762cd121c55bdd523140f181851e40e8f1cfa4d
Kindane <NULL>
2022-03-28 07:50:08 +0300

    Added screenshots, modified <4> option, added preview of lessons and schedules

--- timetable.py
+++ timetable.py
@@ -1,4 +1,4 @@
-# usr/bin/python3.8
+#!/bin/env python
 # python3 --version = 3.8.5
 # GCC -v = 9.3.0 on linux
 # author Kindane
@@ -17,48 +17,96 @@ def clear_console():
         system_command("clear")
     return True
 
+def convert_day(string):
+    tmp = {
+            "monday": "понедельник",
+            "tuesday": "вторник",
+            "wednesday": "среда",
+            "thursday": "четверг",
+            "friday": "пятница",
+    }
+    return tmp[string]
+
 
 time_format = "%H:%M"
+
+
+print("""Что ты хочешь узнать?
+1 - Расписание уроков на неделю
+2 - Расписание уроков на сегодня
+3 - Расписание звонков
+4 - Когда закончится этот урок/перерыв и какой следующий урок? """)
+choice = int(input(": "))
+
 
 try:
     with open("config.json") as file:
         config = json_load(file)
-    today = config["timetable"][datetime.now().strftime("%A").lower()]
+    if choice == 2 or choice == 4:
+        today = config["уроки"][convert_day(datetime.now().strftime("%A").lower())]
+        #today = config["уроки"]["понедельник"] # DEBUG
+        if today != config["уроки"]["пятница"]:
+            callShedule = config["звонки"]
+        else:
+            callShedule = config["звонкиПт"]
 except FileNotFoundError:
     print(pwd())
     print("Я не могу найти config.json.\nПожалуйста, прочитайте файл README.md")
-    print("I can't find config.json.\nPlease, read README.md file")
-    input()
     exit(-1)
 except KeyError:
-    print("Сегодня выходной (today is day off)") 
-    input()
-    exit(-2)
-except Exception as e:
-    print(f"Exception: {e}")
-    input()
-    exit(-3)
-
-print("Что ты хочешь узнать?\n1 - расписание уроков на сегодня\n2 - Когда закончится этот урок/перерыв и какой следующий урок?")
-choice = input(": ")
+    print("Сегодня выходной")
+    exit(0)
 
 
-if today != config["timetable"]["friday"]:
-    callShedule = config["callSchedule"]
-else:
-    callShedule = config["callScheduleFriday"]
 
-if choice == '1':
+
+if choice == 1:
+    clear_console()
+    daynames = ("понедельник", "вторник", "среда", "четверг", "пятница")
+    # Print the names of the columns.
+    print ("   {:<20} {:<20} {:<20} {:<20} {:<20}\n".format(*daynames))
+    tmp = list()
+    num_of_lesson = 0
+    for i in range(8):
+        for day in daynames:
+            try:
+                tmp.append(config["уроки"][day][num_of_lesson])
+            except IndexError:
+                tmp.append("")
+        print(num_of_lesson+1, ". ", end="", sep="")
+        print ("{:<20} {:<20} {:<20} {:<20} {:<20}".format(*tmp))
+        tmp = list()
+        num_of_lesson += 1
+
+
+if choice == 2:
     clear_console()
     for i in range(len(today)):
         lesson = today[i]
-        print(f"{lesson}", end="")
+        print(f"{i+1}. {lesson}", end="")
         # i don't know how, but this works pretty good :D
         print(f"{callShedule[i]}".rjust(30-len(lesson)))
 
-elif choice == '2':
+elif choice == 3:
+    clear_console()
+    x = zip(config["звонки"], config["звонкиПт"])
+    print ("   {:<20} {:<20}".format("Пн-Чт", "Пт"))
+    for num, i in enumerate(tuple(x), 1):
+        print(num, ". ", sep="", end="")
+        print ("{:<20} {:<20}".format(i[0], i[1]))
+    
+
+elif choice == 4:
     clear_console()
     now = datetime.strptime(datetime.now().strftime(time_format), time_format)
+    if now > datetime.strptime(callShedule[-1].split("-")[1], time_format):
+        print("Уроки уже закончились, отдыхай!")
+        exit()
+    if now < datetime.strptime(callShedule[0].split("-")[0], time_format):
+        print("{0} начнётся через {1} минут(ы)".format(
+            today[0], int((datetime.strptime(callShedule[0].split("-")[0],
+                time_format) - now).seconds/60)))
+        exit()
     lesson_number = -1
     is_coffe_break = False
     for i in range(len(today)):
@@ -90,5 +138,3 @@ elif choice == '2':
     else:
         print("Следующий урок: {}".format(today[lesson_number+1]))
         print("Начало урока через: {} минут(ы)".format(int((next_start_lesson-now).seconds/60)))
-
-input("\nPress Enter to exit")
